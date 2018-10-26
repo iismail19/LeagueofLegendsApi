8da49cf8796cefd448c4d0794d72d6195ad89911
iismail1997 <2015.ismail@gmail.com>
2018-10-26 10:50:57 -0500

    Using JSONObejct - successful

--- src/com/ismail/Main.java
+++ src/com/ismail/Main.java
@@ -1,13 +1,17 @@
 package com.ismail;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Main {
     // Send Get Request
-    public static void main(String[] args) throws Exception{
+    public static void test(String[] args) throws Exception{
         URL url = new URL("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-86d89a5d-3b70-457b-adc6-610afab42ba7");
         HttpURLConnection newConnection = (HttpURLConnection) url.openConnection();
 
@@ -30,4 +34,21 @@ public class Main {
         System.out.println(content);
         newConnection.disconnect();
     }
+
+    public static void main(String[] args){
+        Requests httpRequest = new Requests();
+        JSONObject resutls;
+        try {
+            resutls = httpRequest.getRequest(httpRequest.requestSummonerByName("DeathDragon97"));
+            System.out.println(resutls.get("summonerLevel").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
--- src/com/ismail/Requests.java
+++ src/com/ismail/Requests.java
@@ -1,9 +1,12 @@
 package com.ismail;
 
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.io.*;
 import java.util.*;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 // download dependency here:  http://chillyfacts.com/download-java-json-jar/
@@ -11,5 +14,21 @@ import org.json.JSONObject;
 public class Requests extends LolRequests{
     // TODO learn what each Request code stand for... and how to respond according to requestCode
 
+    public JSONObject getRequest(String requestURL) throws IOException, JSONException {
+        URL link = new URL(requestURL);
+        HttpURLConnection connect = (HttpURLConnection) link.openConnection();
+        // response code: tells us if the request url is successful
+        int responeCode = connect.getResponseCode();
+        // returns the response from the link
+        BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+        String line;
+        StringBuffer response = new StringBuffer();
+        while ((line = in.readLine()) != null){
+            response.append(line);
+        } in.close();
+
+        JSONObject responseData = new JSONObject(response.toString());
+        return  responseData;
+    }
 
 }
